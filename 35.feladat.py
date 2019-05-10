cefd4138817c38251062db7dda51b6e86a08242f
mkrisz98 <49514479+mkrisz98@users.noreply.github.com>
2019-05-10 18:22:39 +0200

    Update 35.feladat.py

--- 35.feladat.py
+++ 35.feladat.py
@@ -1,8 +1,9 @@
-string1 = "ejifhsdjhgdsghdebrecendshjq dhfdgds dsjfds zebddebrecenh hxz"
-string2 = "debrecen"
-elofordulas = 0
-x = len(string2)
-for i in range(len(string1)):
-    if string1[i:i+x] == string2:
-        elofordulas = elofordulas + 1
-print("String2 előfordulása String1-ben:",elofordulas)
+def elofordulasok(string1,string2):
+    elofordulas = 0
+    x = len(string2)
+    for i in range(len(string1)):
+        if string1[i:i + x] == string2:
+            elofordulas = elofordulas + 1
+    return elofordulas
+print(elofordulasok("ejifhsdjhgdsghdebrecendshjq dhfdgds dsjfds zebddebrecenh hxz","debrecen"))
+
